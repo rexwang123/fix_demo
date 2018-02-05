ef5e61b0ea36e71a09b7a9cc27b372657174c47e
Jiayi Wang <jiayiwang@jiayis-mbp.attlocal.net>
2018-02-04 20:38:03 -0800

    new change

---
+++ flix_demo/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  flix_demo
+//
+//  Created by Jiayi Wang on 2/4/18.
+//  Copyright © 2018 Jiayi Wang. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var overviewLable: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
